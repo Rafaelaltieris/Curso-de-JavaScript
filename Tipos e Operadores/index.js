f3ee7aa171c4cc23b48a1aa483dd570b8030d7cc
Rafael <raphaelaltieris@gmail.com>
2023-12-23 21:20:53 -0300

    first commit

---
+++ Tipos e Operadores/index.js
@@ -0,0 +1,61 @@
+
+//let numero = 42;
+//alert(numero+10);
+//alert(typeof numero);
+
+/*Arrays
+
+let array = [
+    "Senai",
+    false,
+    "Alunos de Brotas",
+    {n: "Junior"}
+];
+
+alert(array[1]);
+*/
+
+/*Objetos
+
+var objeto = {
+    chave: "valor",
+    a: "Maker",
+    b:120,
+    c:false
+};
+
+alert(objeto.c);
+*/
+
+/* Tipos
+var booleano = false;
+alert(booleano);
+
+var nulo = null;
+alert(nulo);
+
+var nome;
+alert(undefined);
+ */
+
+/*Igual e exatamente igual
+var a = 1;
+var b = -1;
+
+if (a == b) {
+    alert("Verdadeiro .");
+}else {
+    alert("Falso .");
+}
+*/
+
+//Operador Diferente
+
+const n1 = 1;
+const n2 = 1;
+
+if(n1 != n2) {
+    alert("Verdadeiro");
+} else {
+    alert("Falso");
+}
---
+++ aula01-variaveis-if-else/index.js
@@ -0,0 +1,10 @@
+var valor = 3.14;
+const pi = 3.14;
+
+var idade = 29;
+
+if(nome == pi) {
+    alert("O pi está correto");
+}else{
+    alert("o pi não é correto");
+}
